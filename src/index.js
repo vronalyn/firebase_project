d105e64cc532d8768a69d962b70d1d5150a40d73
vronalyn <vronalyn@email.com>
2023-04-03 15:23:15 +0800

    added fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword,  createUserWithEmailAndPassword } from "firebase/auth";
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app";
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
+//import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword,  createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +25,58 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 console.log(app)
+
+const googleSignInBtn = document.querySelector('.google-signIN');
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+document.getElementById("signup-btn").addEventListener("click", function() {
+    var username = document.getElementById("signup-uname").value;
+    var email= document.getElementById("signup-email").value;
+    var password = document.getElementById("signup-password").value;
+
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        const user = userCredential.user;
+        //console.log(user);
+        alert("Signed up Successful!", user);
+        window.location.href = "./main.html";
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log(errorMessage);
+        alert(error);
+    });
+});
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    var email = document.getElementById("login-email").value;
+    var password = document.getElementById("login-password").value;
+
+    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        const user = userCredential.user;
+        //console.log(user);
+        alert("Welcome back! "+user.email);
+        window.location.href = "./main.html";
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        
+        console.log(errorMessage);
+        alert(errorMessage);
+    });
+});
+
+googleSignInBtn.addEventListener('click', () => {
+    signInWithPopup(auth, provider).then((result) => {
+       const user = result.user;
+       alert(`Hello ${user.displayName}! You signed in using Google.`); 
+       window.location.href = "./main.html";
+    }).catch((error) => {
+        const errorMessage = error.message;
+        alert(`Error: ${errorMessage}!`);
+    });
+});
+
